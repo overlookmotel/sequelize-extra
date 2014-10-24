40e187851a0f4ebb3c56a195224016359cbe4fc6
overlookmotel <theoverlookmotel@gmail.com>
2014-10-24 22:52:21 +0100

    Tests for remove unnecessary through table IDs

--- test/all.test.js
+++ test/all.test.js
@@ -25,9 +25,13 @@ describe(Support.getTestDialectTeaser('Tests'), function () {
 		beforeEach(function() {
 			this.User = this.sequelize.define('User', {name: Sequelize.STRING});
 			this.Task = this.sequelize.define('Task', {name: Sequelize.STRING});
+			this.TaskWorker = this.sequelize.define('TaskWorker', {}, {tableName: 'TasksWorkers'});
 			
-			this.Task.belongsTo(this.User);
-			this.User.hasMany(this.Task);
+			this.Task.belongsTo(this.User, {as: 'Owner'});
+			this.User.hasMany(this.Task, {as: 'OwnedTasks', foreignKey: 'OwnerId'});
+			
+			this.Task.hasMany(this.User, {as: 'Workers', through: this.TaskWorker});
+			this.User.hasMany(this.Task, {as: 'WorkTasks', through: this.TaskWorker});
 			
 			return Promise.bind(this).then(function() {
 				return this.sequelize.sync();
@@ -38,65 +42,87 @@ describe(Support.getTestDialectTeaser('Tests'), function () {
 				return this.Task.create({name: 'Washing'});
 			}).then(function(task) {
 				this.washing = task;
-				return this.washing.setUser(this.bob);
+				return this.washing.setOwner(this.bob);
+			}).then(function() {
+				return this.washing.addWorker(this.bob);
 			});
 		});
 		
 		it('Sequelize.getValues(item)', function() {
 			return Promise.bind(this).then(function() {
-				return this.Task.findAll({where: {name: 'Washing'}, include: [this.User]});
+				return this.Task.findAll({where: {name: 'Washing'}, include: [{model: this.User, as: 'Owner'}]});
 			}).then(function(items) {
 				var values = Sequelize.getValues(items);
 				
 				expect(values.dataValues).not.to.exist;
 				expect(values[0]).to.be.ok;
 				expect(values[0].dataValues).not.to.exist;
-				expect(values[0].User).to.be.ok;
-				expect(values[0].User.dataValues).not.to.exist;
-				expect(values[0].User.id).to.equal(this.bob.id);
+				expect(values[0].Owner).to.be.ok;
+				expect(values[0].Owner.dataValues).not.to.exist;
+				expect(values[0].Owner.id).to.equal(this.bob.id);
 			});
 		});
 		
 		it('Instance#getValues()', function() {
 			return Promise.bind(this).then(function() {
-				return this.Task.find({where: {name: 'Washing'}, include: [this.User]});
+				return this.Task.find({where: {name: 'Washing'}, include: [{model: this.User, as: 'Owner'}]});
 			}).then(function(item) {
 				var values = item.getValues();
 				
 				expect(values.dataValues).not.to.exist;
-				expect(values.User).to.be.ok;
-				expect(values.User.dataValues).not.to.exist;
-				expect(values.User.id).to.equal(this.bob.id);
+				expect(values.Owner).to.be.ok;
+				expect(values.Owner.dataValues).not.to.exist;
+				expect(values.Owner.id).to.equal(this.bob.id);
 			});
 		});
 		
-		it('Sequelize.getValuesDedup(item)', function() {
-			return Promise.bind(this).then(function() {
-				return this.Task.findAll({where: {name: 'Washing'}, include: [this.User]});
-			}).then(function(items) {
-				var values = Sequelize.getValuesDedup(items);
-				
-				expect(values.dataValues).not.to.exist;
-				expect(values[0]).to.be.ok;
-				expect(values[0].dataValues).not.to.exist;
-				expect(values[0].UserId).not.to.exist;
-				expect(values[0].User).to.be.ok;
-				expect(values[0].User.dataValues).not.to.exist;
-				expect(values[0].User.id).to.equal(this.bob.id);
+		describe('one-to-many join', function() {
+			it('Sequelize.getValuesDedup(item)', function() {
+				return Promise.bind(this).then(function() {
+					return this.Task.findAll({where: {name: 'Washing'}, include: [{model: this.User, as: 'Owner'}]});
+				}).then(function(items) {
+					var values = Sequelize.getValuesDedup(items);
+					
+					expect(values.dataValues).not.to.exist;
+					expect(values[0]).to.be.ok;
+					expect(values[0].dataValues).not.to.exist;
+					expect(values[0].OwnerId).not.to.exist;
+					expect(values[0].Owner).to.be.ok;
+					expect(values[0].Owner.dataValues).not.to.exist;
+					expect(values[0].Owner.id).to.equal(this.bob.id);
+				});
+			});
+		
+			it('Instance#getValuesDedup()', function() {
+				return Promise.bind(this).then(function() {
+					return this.Task.find({where: {name: 'Washing'}, include: [{model: this.User, as: 'Owner'}]});
+				}).then(function(item) {
+					var values = item.getValuesDedup();
+					
+					expect(values.dataValues).not.to.exist;
+					expect(values.OwnerId).not.to.exist;
+					expect(values.Owner).to.be.ok;
+					expect(values.Owner.dataValues).not.to.exist;
+					expect(values.Owner.id).to.equal(this.bob.id);
+				});
 			});
 		});
 		
-		it('Instance#getValuesDedup()', function() {
-			return Promise.bind(this).then(function() {
-				return this.Task.find({where: {name: 'Washing'}, include: [this.User]});
-			}).then(function(item) {
-				var values = item.getValuesDedup();
-				
-				expect(values.dataValues).not.to.exist;
-				expect(values.UserId).not.to.exist;
-				expect(values.User).to.be.ok;
-				expect(values.User.dataValues).not.to.exist;
-				expect(values.User.id).to.equal(this.bob.id);
+		describe('many-to-many join', function() {
+			it('Instance#getValuesDedup()', function() {
+				return Promise.bind(this).then(function() {
+					return this.Task.find({where: {name: 'Washing'}, include: [{model: this.User, as: 'Workers'}]});
+				}).then(function(item) {
+					var values = item.getValuesDedup();
+					
+					expect(values.Workers).to.be.ok;
+					expect(values.Workers.length).to.equal(1);
+					expect(values.Workers[0].dataValues).not.to.exist;
+					expect(values.Workers[0].id).to.equal(this.bob.id);
+					
+					expect(values.Workers[0].TaskWorker.UserId).not.to.exist;
+					expect(values.Workers[0].TaskWorker.TaskId).not.to.exist;
+				});
 			});
 		});
 	});
